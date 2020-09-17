0c78bff77d27263eb3c8a08cfa12a6dc7c6cb8d9
Sidney Miranda <sidneysmiranda@hotmail.com>
2020-09-17 02:00:45 -0300

    Created home page

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
-
+import Home from './pages/Home';
 function App() {
   return (
+    <>
     <Header />
+    <Home />
+    </>
     );
 }
 
