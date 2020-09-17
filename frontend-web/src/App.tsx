7de218ea4bc18dff0f8934e792475f8d9ccb95a4
Sidney Miranda <sidneysmiranda@hotmail.com>
2020-09-17 00:55:54 -0300

    Adding header component

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header';
+
 function App() {
   return (
-    <h1>Semana DevSuperior</h1>
-  );
+    <Header />
+    );
 }
 
 export default App;
