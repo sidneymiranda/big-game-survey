27a04fd9be3bc3ad8aba0a585919b3800da517e3
Sidney Miranda <sidneysmiranda@hotmail.com>
2020-09-20 18:35:18 -0300

    React native app finished

--- front-mobile/src/pages/CreateRecord/index.tsx
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -1,23 +1,211 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import Header from '../../components/Header';
+import React, { useState, useEffect } from "react";
+import { FontAwesome5 as Icon } from '@expo/vector-icons'
+import { StyleSheet, View, Text, Alert } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import Header from "../../components/Header";
+import PlatformCard from "./PlatformCard";
+import { GamePlatform, Game } from "./types";
+import RNPickerSelect from "react-native-picker-select";
+import axios from 'axios';
+import { RectButton } from 'react-native-gesture-handler';
 
-const CreateRecord = () => {
-  return (
-    <>
-    <Header />
-    <Text style={styles.text}>Hello!</Text>
-    </>
-  )
+const placeholder = {
+  label: "Selecione o Game",
+  value: null
+};
+
+const BASE_URL = 'http://192.168.0.123:8080';
+
+const mapSelectValues = (games: Game[]) => {
+  return games.map(game => ({
+    ...game,
+    key: game.id,
+    label: game.name,
+    value: game.id
+  }));
 }
 
-const styles = StyleSheet.create({
-  text: {
-    color: '#FFF',
-    fontSize: 50,
-    textAlign: 'center',
-    marginTop: 100
+const CreateRecord = () => {
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [platform, setPlatform] = useState<GamePlatform>();
+  const [selectedGame, setSelectedGame] = useState('');
+  const [allGames, setAllGames] = useState<Game[]>([]);
+  const [filteredGames, setFilteredGames] = useState<Game[]>([]);
+
+  const handleChangePlatform = (selectedPlaform: GamePlatform) => {
+    setPlatform(selectedPlaform);
+    const gamesByPlatform = allGames.filter(
+      game => game.platform === selectedPlaform
+    )
+    setFilteredGames(gamesByPlatform);
+  };
+
+  const hadleSubmit = () => {
+    const payload = {name, age, gameId: selectedGame};
+
+    axios.post(`${BASE_URL}/records`, payload)
+      .then(() => {
+        Alert.alert('Dados salvos com sucesso!');
+        setName('');
+        setAge('');
+        setSelectedGame('');
+        setPlatform(undefined);
+      })
+      .catch(() => Alert.alert('Erro ao salvar as informações!'))
   }
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/games`)
+      .then(response => {
+        const selectValues = mapSelectValues(response.data);
+        console.log(selectValues)
+        setAllGames(selectValues);
+      })
+      .catch(() => Alert.alert('Erro ao listar os games!'))
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <View style={styles.container}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Nome"
+          placeholderTextColor="#9E9E9E"
+          onChangeText={text => setName(text)}
+          value={name}
+        />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Idade"
+          placeholderTextColor="#9E9E9E"
+          keyboardType="numeric"
+          maxLength={3}
+          onChangeText={text => setAge(text)}
+          value={age}
+        />
+        <View style={styles.platformContainer}>
+          <PlatformCard
+            platform="PC"
+            icon="laptop"
+            onChange={handleChangePlatform}
+            activePlatform={platform}
+          />
+          <PlatformCard
+            platform="PLAYSTATION"
+            icon="playstation"
+            onChange={handleChangePlatform}
+            activePlatform={platform}
+          />
+          <PlatformCard
+            platform="XBOX"
+            icon="xbox"
+            onChange={handleChangePlatform}
+            activePlatform={platform}
+          />
+        </View>
+        <RNPickerSelect
+          onValueChange={(value) => {
+            setSelectedGame(value);
+          }}
+          placeholder={placeholder}
+          value={selectedGame}
+          items={filteredGames}
+          style={pickerSelectStyle}
+          Icon={() => {
+            return <Icon name="chevron-down" color="#9E9E9E" size={25} />
+          } }
+        />
+        <View style={styles.footer}>
+          <RectButton style={styles.button} onPress={hadleSubmit}>
+            <Text style={styles.buttonText}>
+              SALVAR
+            </Text>
+          </RectButton>
+
+        </View>
+      </View>
+    </>
+  );
+};
+
+const pickerSelectStyle = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    color: '#ED7947',
+    paddingRight: 30,
+    fontFamily: "Play_700Bold",
+    height: 50
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    color: '#ED7947',
+    paddingRight: 30,
+    fontFamily: "Play_700Bold",
+    height: 50
+  },
+  placeholder: {
+    color: '#9E9E9E',
+    fontSize: 16,
+    fontFamily: "Play_700Bold",
+  },
+  iconContainer: {
+    top: 10,
+    right: 12,
+  }
+})
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: "15%",
+    paddingRight: "5%",
+    paddingLeft: "5%",
+    paddingBottom: 50,
+  },
+  inputText: {
+    height: 50,
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    color: "#ED7947",
+    fontFamily: "Play_700Bold",
+    fontSize: 16,
+    paddingLeft: 20,
+    marginBottom: 21,
+  },
+  platformContainer: {
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  footer: {
+    marginTop: "15%",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#00D4FF",
+    flexDirection: "row",
+    borderRadius: 10,
+    height: 60,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontFamily: "Play_700Bold",
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "#0B1F34",
+  },
 });
 
 export default CreateRecord;
