56d7c5cae4b5ec820a02942ef48d0fe819e94a11
Sidney Miranda <sidneysmiranda@hotmail.com>
2020-09-20 18:45:52 -0300

    configuring frontend for production

--- frontend-web/src/pages/Charts/index.tsx
+++ frontend-web/src/pages/Charts/index.tsx
@@ -23,7 +23,7 @@ const initialPieData = {
   series: []
 };
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-sidney.herokuapp.com';
 
 const Charts = () => {
   const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
--- frontend-web/src/pages/Records/index.tsx
+++ frontend-web/src/pages/Records/index.tsx
@@ -8,7 +8,7 @@ import Filters from '../../components/Filters';
 
 import "./styles.css";
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-sidney.herokuapp.com';
 
 const Records = () => {
   const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
