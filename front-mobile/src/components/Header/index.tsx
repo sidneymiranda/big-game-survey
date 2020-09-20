575118ff23b0070ad81fe99f424af21a35b1b334
Sidney Miranda <sidneysmiranda@hotmail.com>
2020-09-20 15:02:27 -0300

    Adding routes in React native

--- front-mobile/src/components/Header/index.tsx
+++ front-mobile/src/components/Header/index.tsx
@@ -1,43 +1,53 @@
-import React from 'react';
-import { Text, StyleSheet, View, Image } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, Image } from "react-native";
+import { TouchableNativeFeedback } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 const Header = () => {
+
+  const navigation = useNavigation();
+
+  const hadleOnPress = () => {
+    navigation.navigate('Home');
+  }
+
   return (
-    <View style={styles.header}>
-      <Image source={require('../../assets/logo.png')} />
-      <Text style={styles.textLogo1}>Big Game</Text>
-      <Text style={styles.textLogo2}>Survey</Text>
-    </View>
-  )
+    <TouchableNativeFeedback onPress={hadleOnPress}>
+      <View style={styles.header}>
+        <Image source={require("../../assets/logo.png")} />
+        <Text style={styles.textLogo1}>Big Game</Text>
+        <Text style={styles.textLogo2}>Survey</Text>
+      </View>
+    </TouchableNativeFeedback>
+  );
 };
 
 const styles = StyleSheet.create({
   header: {
     paddingTop: 50,
     height: 90,
-    backgroundColor: '#37474F',
-    flexDirection: 'row',
-    justifyContent: 'center'
+    backgroundColor: "#37474F",
+    flexDirection: "row",
+    justifyContent: "center",
   },
   textLogo1: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 18,
     fontFamily: "Play_700Bold",
-    color: '#ED7947',
+    color: "#ED7947",
     marginLeft: 10,
     marginRight: 5,
   },
   textLogo2: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontFamily: "Play_700Bold",
     fontSize: 18,
-    color: '#FFF'
+    color: "#FFF",
   },
   tinyLogo: {
     width: 25,
     height: 25,
   },
 });
-
 
 export default Header;
